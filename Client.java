a1e471fb325dcf0eb7fd820add8149bee6b8fec4
ShinnDing <76138405+ShinnDing@users.noreply.github.com>
2022-08-23 15:53:33 -0500

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,98 @@
+package com.shinnding;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * Client class creates and sends client socket requests for Favorite Guitars 1
+ * through 4.
+ * 
+ * @author Stephanie Shinn
+ *
+ */
+public class Client {
+
+	ObjectOutputStream oos = null;
+	ObjectInputStream ois = null;
+
+	/**
+	 * Main method to run Client class.
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		//code removed to adhere to school policy
+	}
+	
+	/**
+	 * Creates the client socket to send requests and receive responses through.
+	 * 
+	 * @param hostName
+	 * @param portNumber
+	 * @param request
+	 */
+	public void sendRequest(String hostName, int portNumber, String request) {
+
+		try {
+		//code removed to adhere to school policy
+
+		} catch (IOException e) {
+			// ignore and let thread die
+		} catch (ClassNotFoundException e) {
+			// ignore and let thread die
+		} finally {
+			try {
+				if (oos != null) {
+					oos.close();
+				}
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (IOException e) {
+				// ignore and let thread die
+			}
+		}
+
+	}
+	
+	/**
+	 * Creates client requests for Favorite Guitars 1 through 4 to pass to Server
+	 * socket.
+	 * 
+	 * @param client
+	 * 
+	 * @throws InterruptedException
+	 */
+	private static void simulateMulipleClients(Client client) throws InterruptedException {
+
+				//code removed to adhere to school policy
+		
+		}
+
+		System.out.println("Testing done.");
+
+		// Send the final request to stop
+		client.sendRequest("localhost", 12001, "stop");
+	}
+	
+	/**
+	 * The request will await its turn before entering socket.
+	 * 
+	 * Adapted from Paul Hyde's "Waiting Best Practices" as taught in ICS 440.
+	 * 
+	 * @param msTimeout
+	 * @param request
+	 * 
+	 * @return boolean
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static boolean waitUntilZero(long msTimeout, String request) throws InterruptedException {
+			//code removed to adhere to school policy
+		}
+	}
+
+}
---
+++ Server.java
@@ -0,0 +1,112 @@
+package com.shinnding;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+/**
+ * Server class to respond to client requests.
+ * 
+ * 
+ * @author Stephanie Shinn
+ *
+ */
+public class Server {
+
+	ServerSocket serverSocket = null;
+	ObjectOutputStream oos = null;
+	ObjectInputStream ois = null;
+	boolean shutdown = false;
+	
+	/**
+	 * Main method to run Server class.
+	 * 
+	 * @args String[] args
+	 * 
+	 * @param Server server
+	 */
+	public static void main(String[] args) {
+		Server server = new Server();
+		try {
+			server.start(12001);
+		} catch (IOException e) {
+			// ignore and let thread die
+		}
+
+	}
+	
+	/**
+	 * Start a server at the given port.
+	 * 
+	 * @param port
+	 * 
+	 * @throws IOException
+	 */
+	public void start(int port) throws IOException {
+
+		//code removed to adhere to school policy
+
+	}
+
+	/**
+	 * Starts thread to process client requests.
+	 * 
+	 * @param port
+	 * @param serverSocket
+	 * 
+	 * @throws IOException
+	 */
+	private void runServer(int port, ServerSocket serverSocket) throws IOException {
+
+		//code removed to adhere to school policy
+
+		try {
+		//code removed to adhere to school policy
+		} catch (SocketException ex) {
+			// Do nothing and let thread die.
+		} catch (Exception ex) {
+			// Do nothing and let thread die.
+		} finally {
+			// Do nothing and let thread die.
+		}
+	}
+	
+	/**
+	 * Creates new ObjectOutputStream and ObjectInputStream to communicate with client socket.
+	 * 
+	 * @param clientSocket
+	 */
+	private void processClient(Socket clientSocket) {
+
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+
+		try {
+			//code removed to adhere to school policy
+		} catch (IOException | ClassNotFoundException e) {
+			// ignore and let thread die
+		} finally {
+			try {
+				if (oos != null) {
+					oos.close();
+				}
+				if (ois != null) {
+					ois.close();
+				}
+				
+				clientSocket.close();
+				
+				if (shutdown) {
+					serverSocket.close();
+				}
+
+			} catch (IOException e) {
+				// ignore and let thread die
+			}
+		}
+	}
+
+}
